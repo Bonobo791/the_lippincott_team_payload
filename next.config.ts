0a9f6f4bce2f13d98dab080ab46e13ff1ce3ed33
Bonobo791 <andrew.weilbacher@gmail.com>
2026-07-23 10:01:47 -0400

    Add outputFileTracingIncludes for sharp-libvips dependency in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -48,6 +48,11 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: true,
   redirects,
+  outputFileTracingIncludes: {
+    '/*': [
+      './node_modules/.pnpm/sharp@*/node_modules/@img/sharp-libvips-linux-x64/lib/**/*',
+    ],
+  },
   turbopack: {
     root: path.resolve(dirname),
   },
